cde2d316b6aa594b34f8d03e279abb4f433e93b5
calmhouse <calmhouse@users.noreply.github.com>
2019-11-16 21:46:12 -0500

    Fix monitoring_init.sh

--- scripts/monitoring_init.sh
+++ scripts/monitoring_init.sh
@@ -1,3 +1,6 @@
+# Stop if error
+set -e
+
 # Use on master node
 kubectl apply -f dashboard.yaml
 kubectl apply -f new_user.yaml
@@ -8,12 +11,12 @@ wget https://get.helm.sh/helm-v2.16.1-linux-amd64.tar.gz
 tar -zxvf helm-v2.16.1-linux-amd64.tar.gz
 
 # Install helm
-mv linux-amd64/helm /usr/local/bin/helm
+sudo mv linux-amd64/helm /usr/local/bin/helm
 helm init
 kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin  --serviceaccount=kube-system:tiller
 kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
-kubectl apply -f crb-kubelet-api-admin.yaml
+kubectl apply -f crb-kubelet-api-admin.yaml --validate=false
 
 # Installs the metrics server
 helm install --name metrics-server stable/metrics-server --namespace metrics --set args={"--kubelet-insecure-tls=true, --kubelet-preferred-address-types=InternalIP\,Hostname\,ExternalIP"}
