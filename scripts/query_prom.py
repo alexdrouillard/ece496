64001dff577560158c119b642a819595e965b581
Alex Drouillard <cooldude@Alexs-MacBook-Air.local>
2019-11-18 13:18:50 -0500

    change defualt to range query over the last 10 hours

--- scripts/query_prom.py
+++ scripts/query_prom.py
@@ -1,33 +1,65 @@
 import requests
 import json
+import time
 
 
 def make_query(query):
     url = "http://142.150.199.211:30001/api/v1/query"
 
-    response = requests.get(
-        f"{url}", params={"query": query}
-    )
+    response = requests.get(f"{url}", params={"query": query})
+
+    return response.json()
+
+
+def make_range_query(query, start_time=None, duration=10):
+    """
+    Makes a range query from start_time to start_time - duration (hours).
+
+    By default, start_time will be the time this is called.
+
+    """
+    url = "http://142.150.199.211:30001/api/v1/query_range"
+    start_time = time.time() if start_time is None else start_time
+    params = {
+        "query": query,
+        "start": start_time - duration * 60,
+        "end": start_time,
+        "step": 60.0,
+    }
+
+    response = requests.get(f"{url}", params=params)
 
     return response.json()
 
 
 if __name__ == "__main__":
+    data_rate = "[1m]"
+
     queries = [
-        'rate(container_network_receive_packets_total{pod=~".*service.*"}[1000m:1m])',
-        'rate(container_network_transmit_packets_total{pod=~".*service.*"}[1000m:1m])',
-        'rate(container_cpu_usage_seconds_total{pod=~".*service.*"}[1000m:1m])',
-        'rate(container_memory_usage_bytes{pod=~".*service.*"}[1000m:1m])',
+        'rate(container_network_receive_packets_total{pod=~".*service.*"}'
+        + data_rate
+        + ")",
+        'rate(container_network_transmit_packets_total{pod=~".*service.*"}'
+        + data_rate
+        + ")",
+        'rate(container_cpu_usage_seconds_total{pod=~".*service.*"}'
+        + data_rate
+        + ")",
+        'rate(container_memory_usage_bytes{pod=~".*service.*"}'
+        + data_rate
+        + ")",
     ]
 
     query_names = [
-            "network receive packets",
-            "network transmit packets",
-            "cpu usage",
-            "memory usage"
+        "network_receive_packets",
+        "network_transmit_packets",
+        "cpu_usage",
+        "memory_usage",
     ]
 
-    jsons = map(make_query, queries) 
+    jsons = []
+    for query in queries:
+        jsons.append(make_range_query(query))
 
     for json_data, query in zip(jsons, query_names):
         with open(query + ".json", "w+") as f:
